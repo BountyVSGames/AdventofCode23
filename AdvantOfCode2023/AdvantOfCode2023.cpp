3f25ea01639571aa7caafc4bcf7eee0988fd4474
BountyVSGames <191056@buas.nl>
2023-12-01 11:52:58 +0100

    [ADD] Added gitignore

--- AdvantOfCode2023/AdvantOfCode2023.cpp
+++ AdvantOfCode2023/AdvantOfCode2023.cpp
@@ -8,28 +8,6 @@ int main()
 	firstDayResult->RunAssignment();
 
 	delete firstDayResult;
- //   std::string firstExample = "labc2";
-	//int firstValue = 0;
-	//int secondValue = 0;
-
-	//for (int i = 0; i < firstExample.length(); i++)
-	//{
-	//	if (firstValue == 0 && isdigit(firstExample.at(i)))
-	//	{
-	//		std::cout << firstExample[i] << std::endl;
-
-	//		//'0' is char 48. '1' is char 49. So we subtract from it to convert the char number to a int.
-	//		firstValue = firstExample[i] - '0';
-	//		secondValue = firstExample[i] - '0';
-	//	}
-	//	else if (isdigit(firstExample.at(i)))
-	//	{
-	//		std::cout << firstExample[i] << std::endl;
-	//		secondValue = firstExample[i] - '0';
-	//	}
-	//}
-
-	//std::cout << firstValue << secondValue << std::endl;
 }
 
 
